5f4d182808aa3e3d26c8192a3edc900935473880
EliteFan <fan552426811@outlook.com>
2019-04-20 20:03:13 +0800

    finished

--- server.js
+++ server.js
@@ -13,18 +13,56 @@ var server = http.createServer(function (request, response) {
 
     if (path === '/') {
         var string = fs.readFileSync('./index.html', 'utf-8')
-        var amount = fs.readFileSync('./db', 'utf-8')
-        string = string.replace('&&&amount&&&', amount)
+        let cookies =transToObject(request.headers.cookie,'; ')
+        console.log(cookies)
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        string = string.replace('___acount___',cookies.sign_in_email)
+        console.log(string)
         response.write(string)
         response.end()
-    } else if (path === '/css/signup.css') {
-        var string = fs.readFileSync('./css/signup.css', 'utf-8')
+    } else if (path === '/css/sign.css') {
+        var string = fs.readFileSync('./css/sign.css', 'utf-8')
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css')
         response.write(string)
         response.end()
+    } else if (path === '/sign_in') {
+        var string = fs.readFileSync('./sign_in.html', 'utf-8')
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(string)
+        response.end()
+    } else if (path === '/signin' && method.toUpperCase() === 'POST') {
+        getBody(request).then((body) => {
+            response.setHeader('Content-Type', 'application/json;charset=utf-8')
+            let result = transToObject(body,'&')
+            console.log(result)
+            let { email, password } = result
+            if (email.indexOf('@') === -1) {
+                response.statusCode = 400
+                response.write('{"errors":"来自后端：email error"}')
+            } else {
+                let users = fs.readFileSync('./db/users', 'utf-8')
+                users = JSON.parse(users)
+                let pass = false
+                for (let i = 0; i < users.length; i++) {
+                    let user = users[i]
+                    if (user.email === email && user.password === password) {
+                        pass = true
+                    }
+                }
+                if (pass) {
+                    response.statusCode = 200
+                    response.setHeader('Set-Cookie', 'sign_in_email=' + email)
+                    response.write('{"success":"regist success!"}')
+                } else {
+                    response.statusCode = 400
+                    response.write('{"errors":"来自后端：账号不存在或密码不匹配！"}')
+                }
+            }
+            response.end()
+        })
     } else if (path === '/sign_up') {
         var string = fs.readFileSync('./sign_up.html', 'utf-8')
         response.statusCode = 200
@@ -32,31 +70,36 @@ var server = http.createServer(function (request, response) {
         response.write(string)
         response.end()
     } else if (path === '/signup' && method.toUpperCase() === 'POST') {
-        getBody(request).then((body)=>{
-            let result = {}
-            let arr = decodeURIComponent(body).split('&')
-            arr.forEach(element => {
-                let parts = element.split('=')
-                let key = parts[0]
-                let value = parts[1]
-                result[key] = value
-            })
+        getBody(request).then((body) => {
+            response.setHeader('Content-Type', 'application/json;charset=utf-8')
+            let result = transToObject(body,'&')
             console.log(result)
             let { email, password, confirmpsd } = result
             if (email.indexOf('@') === -1 || password !== confirmpsd) {
                 response.statusCode = 400
-                response.setHeader('Content-Type', 'application/json;charset=utf-8')
                 response.write('{"errors":"来自后端：email error or password error"}')
             } else {
-                let users = fs.readFileSync('./db/users','utf8')
+                let users = fs.readFileSync('./db/users', 'utf-8')
+                // 解析JSON字符串为js对象
                 users = JSON.parse(users)
-                users.push({email:email,password:password})
-                let userstring = JSON.stringify(users)
-                fs.writeFileSync('./db/users',userstring)
-                console.log(userstring)
-                response.statusCode = 200
-                response.setHeader('Content-Type', 'application/json;charset=utf-8')
-                response.write('{"success":"regist success!"}')
+                let pass = true
+                for (let i = 0; i < users.length; i++) {
+                    if (users[i].email === email) {
+                        pass = false
+                        break
+                    }
+                }
+                if (pass) {
+                    users.push({ email: email, password: password })
+                    // js对象转化为JSON
+                    let userstring = JSON.stringify(users)
+                    fs.writeFileSync('./db/users', userstring)
+                    response.statusCode = 200
+                    response.write('{"success":"regist success!"}')
+                } else {
+                    response.statusCode = 400
+                    response.write('{"errors":"来自后端：账户已存在！"}')
+                }
             }
             response.end()
         })
@@ -105,6 +148,18 @@ function getBody(request) {
             resolve(body)
         })
     })
+}
+
+function transToObject(body,separator) {
+    let result = {}
+    let arr = decodeURIComponent(body).split(separator)
+    arr.forEach(element => {
+        let parts = element.split('=')
+        let key = parts[0]
+        let value = parts[1]
+        result[key] = value
+    })
+    return result
 }
 server.listen(port)
 console.log('监听 ' + port + '成功，请打开 http://localhost:' + port)
